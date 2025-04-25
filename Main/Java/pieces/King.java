f469929b1522ee5049f5ef4b2b2d3c07edeac232
Kelsey Kiddy <krmellotte@gmail.com>
2025-04-25 00:58:09 -0400

    Add files via upload
    Added rock, paper, scissors game to determine player colors, added clocks. Added more functionality. Castling and pawn promotion are working. Check/Checkmate still needs tested.

---
+++ Main/Java/pieces/King.java
@@ -0,0 +1,38 @@
+// src/main/java/pieces/King.java
+package Main.Java.pieces;
+
+import java.util.ArrayList;
+import java.util.List;
+import Main.Java.types.*;
+import Main.Java.board.*;
+
+public class King extends Piece {
+
+    public King(Team team) {
+        super(team, PieceTypes.KING);
+    }
+
+    //King can move one square at a time in any direction
+    @Override
+    public List<Move> availableMoves(Board board, Coordinate currentCoord) {
+        List<Move> moves = new ArrayList<>();
+        int[] dx = {-1, 0, 1, -1, 1, -1, 0, 1};
+        int[] dy = {-1, -1, -1, 0, 0, 1, 1, 1};
+
+        for (int dir = 0; dir < 8; dir++) {
+            int x = currentCoord.getX() + dx[dir];
+            int y = currentCoord.getY() + dy[dir];
+
+            if (board.isValidCoordinate(x, y)) {
+                Coordinate target = new Coordinate(x, y);
+                Piece targetPiece = board.getPieceAt(target);
+
+                if (targetPiece == null || targetPiece.getTeam() != this.getTeam()) {
+                    moves.add(new Move(currentCoord, target)); //Take piece
+                }
+            }
+        }
+
+        return moves;
+    }
+}
