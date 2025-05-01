a2af72b0ca9b4be6dc2600533d47beb2caaea406
Frostburg-jjuraidini <jjuraidini0@frostburg.edu>
2025-05-01 16:55:40 -0500

    Update Pawn.java

--- Pieces/Pawn.java
+++ Pieces/Pawn.java
@@ -1,3 +1,5 @@
+package Pieces;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,20 +15,20 @@ public class Pawn extends Piece {
         int direction = (this.getTeam() == Team.WHITE) ? 1 : -1;
         int startRow = (this.getTeam() == Team.WHITE) ? 1 : 6;
 
-        // Pawns move one square forward at a time
+        // Move one square forward
         if (board.isValidCoordinate(currentCoord.getX(), currentCoord.getY() + direction) &&
-            board.getPieceAt(new Coordinate(currentCoord.getX(), currentCoord.getY() + direction)) == null) {
+                board.getPieceAt(new Coordinate(currentCoord.getX(), currentCoord.getY() + direction)) == null) {
             moves.add(new Move(currentCoord, new Coordinate(currentCoord.getX(), currentCoord.getY() + direction)));
         }
 
-        // Pawns can move two squares forward if its their first move
+        // Move two squares forward from starting position
         if (currentCoord.getY() == startRow &&
-            board.getPieceAt(new Coordinate(currentCoord.getX(), currentCoord.getY() + direction)) == null &&
-            board.getPieceAt(new Coordinate(currentCoord.getX(), currentCoord.getY() + 2 * direction)) == null) {
+                board.getPieceAt(new Coordinate(currentCoord.getX(), currentCoord.getY() + direction)) == null &&
+                board.getPieceAt(new Coordinate(currentCoord.getX(), currentCoord.getY() + 2 * direction)) == null) {
             moves.add(new Move(currentCoord, new Coordinate(currentCoord.getX(), currentCoord.getY() + 2 * direction)));
         }
 
-        // Pawns take other pieces diagonally
+        // Capture diagonally
         for (int dx = -1; dx <= 1; dx += 2) {
             int x = currentCoord.getX() + dx;
             int y = currentCoord.getY() + direction;
@@ -36,7 +38,7 @@ public class Pawn extends Piece {
                 Piece targetPiece = board.getPieceAt(target);
 
                 if (targetPiece != null && targetPiece.getTeam() != this.getTeam()) {
-                    moves.add(new Move(currentCoord, target)); 
+                    moves.add(new Move(currentCoord, target));
                 }
             }
         }
